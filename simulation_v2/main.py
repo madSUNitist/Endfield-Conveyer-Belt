f7982191aa71df8bdb2ef8965f3fc173d39ff1e0
madSUNitist <caejgbfdiegbfdj@163.com>
2026-03-09 23:19:49 +0800

    Delete simulation_v2 directory

--- simulation_v2/main.py
+++
@@ -1,8 +0,0 @@
-from tests.components.test_converger import TestConverger
-import logging
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    
-    t = TestConverger()
-    t.test_converger()
